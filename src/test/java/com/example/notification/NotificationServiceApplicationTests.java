4589ba675a1f113ad88d6bb3ebb5938ddc981f62
Renato Prata <renprata@hotmail.com>
2025-07-12 12:25:57 -0300

    Fix Application Test

--- src/test/java/com/example/notification/NotificationServiceApplicationTests.java
+++ src/test/java/com/example/notification/NotificationServiceApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.notification;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@TestPropertySource(properties = {
+		"spring.kafka.bootstrap-servers=localhost:9092",
+		"management.zipkin.tracing.endpoint=http://localhost:9411/api/v2/spans"
+	})
 class NotificationServiceApplicationTests {
 
 	@Test
